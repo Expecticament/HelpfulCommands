e05b2266d2d309bad69fdcb7427f9aa845628e6d
ThatsNotM3 <60347034+ThatsNotM3@users.noreply.github.com>
2023-07-16 23:35:00 +0300

    More translations and command overhauls.
    /gm is the first command to receive an actual translation ability, as well as support for specifying target players (just like normal /gamemode).
    Updates for /hc and mod info

--- src/main/java/net/thatsnotm3/helpfulcommands/command/CMD_Gm.java
+++ src/main/java/net/thatsnotm3/helpfulcommands/command/CMD_Gm.java
@@ -1,54 +1,110 @@
 package net.thatsnotm3.helpfulcommands.command;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
 import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.GameMode;
 
 public class CMD_Gm{
+
+    static final String cmdName="gm";
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment){
-        dispatcher.register(CommandManager.literal("gm")
-            .then(CommandManager.literal("c").executes(ctx->switchGameMode(ctx,0)))
-            .then(CommandManager.literal("s").executes(ctx->switchGameMode(ctx,1)))
-            .then(CommandManager.literal("a").executes(ctx->switchGameMode(ctx,2)))
-            .then(CommandManager.literal("sp").executes(ctx->switchGameMode(ctx,3)))
+        dispatcher.register(CommandManager.literal(cmdName)
+            .then(CommandManager.literal("c").executes(ctx->switchGameMode(ctx,0,null))
+                .then(CommandManager.argument("target", EntityArgumentType.players())
+                .executes(ctx->switchGameMode(ctx,0,EntityArgumentType.getPlayers(ctx, "target")))
+                )
+            )
+            .then(CommandManager.literal("s").executes(ctx->switchGameMode(ctx,1,null))
+                .then(CommandManager.argument("target", EntityArgumentType.players())
+                .executes(ctx->switchGameMode(ctx,1,EntityArgumentType.getPlayers(ctx, "target")))
+                )
+            )
+            .then(CommandManager.literal("a").executes(ctx->switchGameMode(ctx,2,null))
+                .then(CommandManager.argument("target", EntityArgumentType.players())
+                .executes(ctx->switchGameMode(ctx,2,EntityArgumentType.getPlayers(ctx, "target")))
+                )
+            )
+            .then(CommandManager.literal("sp").executes(ctx->switchGameMode(ctx,3,null))
+                .then(CommandManager.argument("target", EntityArgumentType.players())
+                .executes(ctx->switchGameMode(ctx,3,EntityArgumentType.getPlayers(ctx, "target")))
+                )
+            )
         );
     }
 
-    public static int switchGameMode(CommandContext<ServerCommandSource> ctx, int gm) throws CommandSyntaxException{ // 0 - Creative; 1 - Survival; 2 - Adventure; 3 - Spectator
+    public static int switchGameMode(CommandContext<ServerCommandSource> ctx, int gm, Collection<ServerPlayerEntity> targets) throws CommandSyntaxException{ // 0 - Creative; 1 - Survival; 2 - Adventure; 3 - Spectator
         ServerPlayerEntity player=ctx.getSource().getPlayer();
-
-        if(!ModCommandManager.RunChecks("gm",player)) return -1;
+        
+        if(!ModCommandManager.RunChecks(cmdName,player)) return -1;
 
         GameMode gameMode;
-        String name;
         switch(gm){
             default:
-                name="Creative";
                 gameMode=GameMode.CREATIVE;
                 break;
             case 1:
-                name="Survival";
                 gameMode=GameMode.SURVIVAL;
                 break;
             case 2:
-                name="Adventure";
                 gameMode=GameMode.ADVENTURE;
                 break;
             case 3:
-                name="Spectator";
                 gameMode=GameMode.SPECTATOR;
                 break;
         }
 
-        player.changeGameMode(gameMode);
-        player.sendMessage(Text.literal("Updated your Game Mode to \u00A76"+name));
+        if(targets!=null){
+            int i=0;
+            Iterator iter=targets.iterator();
+            List<String> targetNames=new ArrayList<String>();
+            while(iter.hasNext()){
+                ServerPlayerEntity target=(ServerPlayerEntity)iter.next();
+                if(target.changeGameMode(gameMode)){
+                    if(target!=player){
+                        MutableText part1=Text.literal("\u00A77"+player.getEntityName()+":\u00A7r ");
+                        MutableText part2=Text.translatable("message.command.gm.self", Text.translatable("gameMode."+gameMode.getName()).formatted(Formatting.GOLD));
+                        MutableText msg=part1.append(part2);
+                        target.sendMessage(msg);
+                    }
+                    targetNames.add(target.getEntityName());
+                    ++i;
+                }
+            }
+            if(i>0){
+                String allTargetNames="";
+                for(String n : targetNames) allTargetNames=allTargetNames+n+"\n";
+                allTargetNames=allTargetNames.substring(0, allTargetNames.length()-1);
+                Style playerList=Style.EMPTY
+                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,Text.literal(allTargetNames)))
+                    .withColor(Formatting.AQUA)
+                ;
+                MutableText msg=Text.translatable("message.command.gm.target",Text.translatable("gameMode."+gameMode.getName()).formatted(Formatting.GOLD),Text.literal(Integer.toString(i)).setStyle(playerList));
+                player.sendMessage(msg);
+            }
+        } else{
+            if(player.changeGameMode(gameMode)){
+                MutableText msg=Text.translatable("message.command.gm.self",Text.translatable("gameMode."+gameMode.getName()).formatted(Formatting.GOLD));
+                player.sendMessage(msg);
+            }
+        }
         return 1;
     }
 }
--- src/main/java/net/thatsnotm3/helpfulcommands/command/CMD_Hc.java
+++ src/main/java/net/thatsnotm3/helpfulcommands/command/CMD_Hc.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.util.Map;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.*;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
@@ -20,16 +22,20 @@ import net.minecraft.text.Text;
 import net.thatsnotm3.helpfulcommands.HelpfulCommands;
 import net.thatsnotm3.helpfulcommands.TranslationManager;
 import net.thatsnotm3.helpfulcommands.util.ConfigUtils;
-import net.fabricmc.loader.api.FabricLoader;
 
 public class CMD_Hc{
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment){
         LiteralArgumentBuilder<ServerCommandSource> commands=CommandManager.literal("commands");
         for(String i : ModCommandManager.commands){
             LiteralArgumentBuilder<ServerCommandSource> literalCommand=CommandManager.literal(i)
-                    .then(CommandManager.argument("value", BoolArgumentType.bool())
-                    .executes(ctx -> toggleCommand(ctx, i, BoolArgumentType.getBool(ctx, "value"))));
-            
+                    .then(CommandManager.literal("toggle")
+                        .then(CommandManager.argument("value", BoolArgumentType.bool())
+                        .executes(ctx -> toggleCommand(ctx, i, BoolArgumentType.getBool(ctx, "value"))))
+                    )
+                    .then(CommandManager.literal("opLevel")
+                        .then(CommandManager.argument("value", IntegerArgumentType.integer(0, 4))
+                        .executes(ctx->changeCommandOpLevel(ctx, i, IntegerArgumentType.getInteger(ctx, "value"))))
+                    );
             commands.then(literalCommand);
         }
         commands.executes(CMD_Hc::cmdList);
@@ -72,15 +78,19 @@ public class CMD_Hc{
 
         return 1;
     }
+    public static int changeCommandOpLevel(CommandContext<ServerCommandSource> ctx,String cmd,int level){
+        return 1;
+    }
 
     public static int info(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException{
         ServerPlayerEntity player=ctx.getSource().getPlayer();
 
+        player.sendMessage(Text.literal("\u00A7a\u00A7l\u00AB Info \u00A7r\u00A76|| Helpful Commands \u00A7a\u00A7l\u00BB"));
         player.sendMessage(Text.literal(
-            "\u00A7a\u00A7lHelpful Commands Mod \u00A7r\u00A77|| ver: "+FabricLoader.getInstance().getModContainer("helpfulcommands").get().getMetadata().getVersion()+" || by ThatsNotM3"+"\u00A7r\n "+FabricLoader.getInstance().getModContainer("helpfulcommands").get().getMetadata().getDescription()
+            " "+FabricLoader.getInstance().getModContainer(HelpfulCommands.modID).get().getMetadata().getDescription()+"\n \u00A77v. "+FabricLoader.getInstance().getModContainer(HelpfulCommands.modID).get().getMetadata().getVersion()+" || by ThatsNotM3"
         ));
         player.sendMessage(Text.literal("\n\u00A7b[Command List]").setStyle(Style.EMPTY
-            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/hc commandList"))
+            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/hc commands"))
             .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,Text.literal("Print Command List")))
         ));
         player.sendMessage(Text.literal("\u00A7b[GitHub]").setStyle(Style.EMPTY
@@ -93,31 +103,15 @@ public class CMD_Hc{
     public static int cmdList(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException{
         ServerPlayerEntity player=ctx.getSource().getPlayer();
         MinecraftServer server=player.getServer();
-
-        /*player.sendMessage(Text.literal(
-            +
-            "\n"+getCommandColor("abilities",server)+" Configure and read your abilities (fly, invulnerability, etc.)"+
-            "\n"+getCommandColor("back",server)+" Return to where you last died"+
-            "\n"+getCommandColor("day",server)+" Set the time to Day"+
-            "\n"+getCommandColor("dimension",server)+" Read or switch your current dimension"+
-            "\n"+getCommandColor("explosion",server)+" Create an explosion"+
-            "\n"+getCommandColor("feed",server)+" Feeds and completely saturates you"+
-            "\n"+getCommandColor("gm",server)+" Switch your game mode"+
-            "\n"+getCommandColor("heal",server)+" Fully restores your health"+
-            "\n"+getCommandColor("home",server)+" Set, get, and teleport to your Home Position"+
-            "\n"+getCommandColor("jump",server)+" Teleport to your cursor position with a given distance"+
-            "\n"+getCommandColor("killitems",server)+" Kill all items lying on the ground"+
-            "\n"+getCommandColor("lightning",server)+" Strike a Lightning at your cursor position"+
-            "\n"+getCommandColor("night",server)+" Set the time to Night"+
-            "\n"+getCommandColor("spawn",server)+" Get or teleport to your or world spawn point"
-        ));*/
-        player.sendMessage(Text.literal("\u00A7b\u00A7lCommand List\u00A7r \u00A77|| \u00A7aEnabled \u00A7cDisabled\u00A7r"));
+        
+        player.sendMessage(Text.literal("\u00A7a\u00A7l\u00AB Command List \u00A7r\u00A76|| Helpful Commands \u00A7a\u00A7l\u00BB"));
+        player.sendMessage(Text.literal("\u00A7e(OP level) \u00A7aEnabled\u00A7r/\u00A7cDisabled\n"));
         for(String i : ModCommandManager.commands){
             player.sendMessage(Text.literal(
-                getCommandColor(i,server)+": "+TranslationManager.getTranslation("command.description."+i)
+                "\u00A7e("+getCommandOpLevel(i,server)+") "+getCommandColor(i,server)+": "+TranslationManager.getTranslation("command.description."+i)
             ).setStyle(Style.EMPTY
                 .withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND,"/"+i))
-                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,Text.literal("Click to suggest command")))
+                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,Text.literal("Click to suggest this command")))
             ));
         }
 
@@ -134,5 +128,8 @@ public class CMD_Hc{
         }
         if(map.containsKey(cmd)) if(!map.get(cmd)) return "\u00A7c/"+cmd+"\u00A77";
         return "\u00A7a/"+cmd+"\u00A77";
+    }
+    static String getCommandOpLevel(String cmd,MinecraftServer server){
+        return "2";
     }
 }
